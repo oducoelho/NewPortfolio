d989ea174b5cde72ed39348ff7c2f9f145b105da
Eduardo Coelho <eduardocoelho619@gmail.com>
2023-05-29 17:54:03 -0300

    removing console logs

--- app/components/pages/home/known-techs/known-tech.tsx
+++ app/components/pages/home/known-techs/known-tech.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { CMSIcon } from "@/app/components/cms-icon"
-import { KnownTech as IKnownTech } from "@/app/types/projects"
 import { getRelativeTimeString } from "@/app/utils/get-relative-time"
+import { KnownTech as IKnownTech } from "@/app/types/projects"
+import { CMSIcon } from "@/app/components/cms-icon"
 
 type KnownTechProps = {
   tech: IKnownTech
@@ -10,7 +10,8 @@ type KnownTechProps = {
 
 export const KnownTech = ({ tech }: KnownTechProps) => {
   const relativeTime = getRelativeTimeString(
-    new Date(tech.startDate), 'pt-BR',
+    new Date(tech.startDate),
+    'pt-BR',
   ).replace('há ', '')
   return (
     <div className="p-6 rounded-lg bg-gray-600/20 text-gray-500 flex flex-col gap-2 hover:text-emerald-500 hover:bg-gray-600/30 transition-all">
